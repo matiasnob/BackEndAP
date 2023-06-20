103543324043b2ecce08ae47b622284440840053
matiasnob <matiasprestifilippo@yahoo.com.ar>
2023-06-20 09:28:44 -0300

    primer commit

---
+++ src/main/java/com/apbackend/apbackend/ApbackendApplication.java
@@ -0,0 +1,13 @@
+package com.apbackend.apbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApbackendApplication.class, args);
+	}
+
+}
